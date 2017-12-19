7d572ab0c7e22cfc52688b6b1f0d3a2d076f2f38
sandeep152 <sandeep152@users.noreply.github.com>
2017-12-19 06:21:03 +0000

    load sample target

--- Targets/TargetTableViewController.swift
+++ Targets/TargetTableViewController.swift
@@ -35,24 +35,30 @@ class TargetTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return Targets.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        
+        // Table view cells are reused and should be dequeued using a cell identifier.
+        let cellIdentifier = "TargetTableViewCell"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? TargetTableViewCell  else {
+            fatalError("The dequeued cell is not an instance of TargetTableViewCell.")
+        }
+        
+        // Fetches the appropriate meal for the data source layout.
+        let target = Targets[indexPath.row]
+        
+        cell.nameLabel.text = target.name
+        cell.photoImageView.image = target.photo
+        
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
